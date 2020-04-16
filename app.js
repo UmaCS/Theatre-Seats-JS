3863b23820ea8d124621cb69a6056dee3f1e57bd
UmaCS <wiutumka@gmail.com>
2020-04-15 18:54:03 -0700

    few

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 const moviesMenu = document.getElementById('movies');
 const seat = document.getElementsByClassName('seat')
 const selectedSeats = document.getElementsByClassName('selected');
-const finalSelected = document.getElementById('selected-seats');
-
-
+const counter = document.getElementById('counter');
+const totalPrice = document.getElementById('total-price');
 
 // *******  Functions  *******
 
-const getSelectedMovie = () => console.log(moviesMenu.value);
-
 for (let i = 0; i < seat.length; i++) {
 	const seatClass = seat[i].classList;
-	let totalSeats = 0;
+	// let totalSeats = 0;
+
 
 	function removeSeat() {
 		seatClass.remove('selected');
 		seatClass.add('available');
-		totalSeats -= 1;
+		counter.textContent--;
+		// totalPrice.textContent ;
+		totalPrice.textContent = (totalPrice.textContent - moviesMenu.value).toFixed(2);
 	}
 
 	function addSeat() {
 		seatClass.add('selected');
 		seatClass.remove('available');
-		totalSeats += 1;
+		counter.textContent++;
+		totalPrice.textContent = (moviesMenu.value * counter.textContent).toFixed(2);
 	}
 
 	seat[i].addEventListener('click', (e) => {
 		// e.preventDefault();
-		if (seatClass[1] === 'selected') {
+		if (moviesMenu.value == 0) {
+			alert('Choose a movie first!');
+		} else if (seatClass[1] === 'selected') {
 			removeSeat();
 
-		} else if (seatClass[1] !== 'selected') {
+		} else if (seatClass[1] !== 'selected' && seatClass[1] !== 'occupied') {
 			addSeat();
 		}
 
-		// selected.length
-		console.log(totalSeats)
 
 	});
 }
